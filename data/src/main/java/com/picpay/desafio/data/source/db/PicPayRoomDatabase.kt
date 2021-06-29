fbfb71e9fed56152546d4e5078f2ce99ebc816bb
arthur.ferreira <arthur.ferreira@iteris.com.br>
2021-06-29 13:26:20 -0300

    removendo comentário

--- data/src/main/java/com/picpay/desafio/data/source/db/PicPayRoomDatabase.kt
+++ data/src/main/java/com/picpay/desafio/data/source/db/PicPayRoomDatabase.kt
@@ -4,10 +4,6 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 import com.picpay.desafio.data.model.local.UserData
 
-/**
- * This is the backend. The database. This used to be done by the OpenHelper.
- * The fact that this has very few comments emphasizes its coolness.
- */
 @Database(
     entities = [UserData::class],
     version = 1,
